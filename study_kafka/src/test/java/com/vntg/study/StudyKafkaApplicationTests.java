7c09ee3b07455f738721639fd09a46880e49baa9
yjkno1 <yjkno1@gmail.com>
2021-01-25 09:36:32 +0900

    Update: kafka project first update

---
+++ study_kafka/src/test/java/com/vntg/study/StudyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vntg.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
